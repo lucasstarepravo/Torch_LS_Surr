b3f3febc30a3a5767dcc27ecc4e7005cb47e18d2
lucasstarepravo <lucas.gerkenstarepravo@postgrad.manchester.ac.uk>
2024-09-13 16:16:42 -0300

    Using os to make address the saving directory and making sure the target directory exists

--- models/SaveNLoad.py
+++ models/SaveNLoad.py
@@ -2,12 +2,18 @@ import torch
 from models.ANN import ANN_topology
 from models.PINN import PINN_topology
 import pickle as pk
+import os
 
 
 def save_model_instance(ann_instance, filepath, model_type, model_ID):
-    # Save the PyTorch model's state dict
-    torch.save(ann_instance.model.state_dict(), filepath + '/' + str(model_type) + str(model_ID)+".pth")
+    # Ensure the directory exists
+    os.makedirs(filepath, exist_ok=True)
 
+    # Save the PyTorch model's state dict
+    model_path = os.path.join(filepath, f'{model_type}{model_ID}.pth')
+    torch.save(ann_instance.state_dict(), model_path)
+
+    # Prepare attributes to save
     attrs = {
         'input_size': ann_instance.input_size,
         'output_size': ann_instance.output_size,
@@ -15,7 +21,9 @@ def save_model_instance(ann_instance, filepath, model_type, model_ID):
         'history': (ann_instance.training_loss, ann_instance.val_loss)
     }
 
-    with open(filepath + '/attrs' + str(model_ID) + '.pk', 'wb') as f:
+    # Save the attributes using pickle
+    attrs_path = os.path.join(filepath, f'attrs{model_ID}.pk')
+    with open(attrs_path, 'wb') as f:
         pk.dump(attrs, f)
 
 
@@ -27,14 +35,22 @@ def load_attrs(filepath):
     return attrs
 
 
-def load_model_instance(filepath, attrs):
+def load_model_instance(filepath, attrs, model_type='ANN'):
     ''' In this case the filepath must contain the complete directory including the file'''
     input_size = attrs['input_size']
     output_size = attrs['output_size']
     hidden_layers = attrs['hidden_layers']
     model_state = torch.load(filepath)
 
-    # In this case it doesn't matter if ANN_topology or PINN_topology is used as they
-    model = ANN_topology(input_size, output_size, hidden_layers)
+    # Load the appropriate model type
+    if model_type == 'ANN':
+        model = ANN_topology(input_size, output_size, hidden_layers)
+    elif model_type == 'PINN':
+        model = PINN_topology(input_size, output_size, hidden_layers)
+    else:
+        raise ValueError("Invalid model_type. Must be 'ANN' or 'PINN'.")
+
+    # Load the saved state dict into the model
     model.load_state_dict(model_state)
+
     return model
