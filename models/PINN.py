e740176a7d5f6d32070cfbffe5a6718c3ac0bb08
Lucas Starepravo <lucas.gerkenstarepravo@postgrad.macnhester.ac.uk>
2024-10-30 16:08:31 +0000

    Implementing CPU ddp with model saving inside fit method

--- models/PINN.py
+++ models/PINN.py
@@ -4,14 +4,16 @@ import torch.nn as nn
 import time
 from tqdm import tqdm
 from torch.distributed import init_process_group
-
-import torch.distributed.autograd as dist_autograd
+import logging
+from torch.nn.modules.utils import consume_prefix_in_state_dict_if_present as consume_pref
 from torch.nn.parallel import DistributedDataParallel as DDP
 import torch.distributed as dist
-import torch.multiprocessing as mp
-from torch import optim
-from torch.distributed.optim import DistributedOptimizer
-from torch.distributed.rpc import RRef
+import os
+import pickle as pk
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 
 class PINN_topology(nn.Module):
@@ -92,6 +94,7 @@ class PINN:
                  train_l, val_f, val_l, moments, dynamic_physics_loss=False,
                  alpha_epoch_start=0, alpha_epoch_stop=None, final_alpha=0.5):
         '''alpha_epoch_stop must be bigger than alpha_epoch_start'''
+        self.loss_function = None
         self.train_loader = None
         self.val_loader = None
         self.optimizer = None
@@ -99,16 +102,16 @@ class PINN:
         self.output_size = train_l.shape[1]
         self.hidden_layers = hidden_layers
         self.optim_choice = optimizer
+        self.lossfunc_choice = loss_function
 
 
-        self.loss_function = define_loss(loss_function)
         self.epochs = epochs
         self.train_f = train_f
         self.train_l = train_l
         self.val_f = val_f
         self.val_l = val_l
 
-
+        self.model = None
         self.best_model_wts = None
         self.training_loss = None
         self.val_loss = None
@@ -132,8 +135,9 @@ class PINN:
             # If alpha is not given a value, it will be only finished incremented in the last epoch
             self.alpha_epoch_stop = epochs
 
-    def fit(self, proc_index, nprocs):
 
+    def fit(self, proc_index, nprocs, path_to_save, model_type, model_ID):
+        logger.debug('Importing backend')
         # Initialising backend
         # If want to use GPU, use nccl backend
         init_process_group(backend='gloo', world_size=nprocs, rank=proc_index)
@@ -141,24 +145,25 @@ class PINN:
         # Enumerating process -- Only necessary for GPU
         #torch.cuda.set_device(proc_index)
 
+        logger.debug('Formating data')
         # Preparing data
         train_tensor = TensorDataset(self.train_f, self.train_l)
         tr_sampler = torch.utils.data.distributed.DistributedSampler(train_tensor,
                                                                      num_replicas=nprocs, rank=proc_index)
         self.train_loader = torch.utils.data.DataLoader(train_tensor,
-                                                        batch_size=self.batch_size, sampler=tr_sampler)
+                                                        batch_size=self.batch_size, sampler=tr_sampler, num_workers=4)
 
         val_tensor = TensorDataset(self.val_f, self.val_l)
         val_sampler = torch.utils.data.distributed.DistributedSampler(val_tensor,
                                                                      num_replicas=nprocs, rank=proc_index)
         self.val_loader = torch.utils.data.DataLoader(val_tensor,
-                                                      batch_size=self.batch_size, sampler=val_sampler)
+                                                      batch_size=self.batch_size, sampler=val_sampler, num_workers=4)
 
         #self.train_loader = DataLoader(TensorDataset(train_f, train_l), batch_size=batch_size,
         #                               shuffle=True)
         #self.val_loader = DataLoader(TensorDataset(val_f, val_l), batch_size=batch_size,
         #                             shuffle=False)
-
+        logger.debug('Initialising model')
         # Initialising model -- append the end if using GPU
         self.model = PINN_topology(self.input_size, self.output_size, self.hidden_layers)#.to(proc_index)
 
@@ -166,13 +171,17 @@ class PINN:
         if self.optim_choice == 'adam':
             self.optimizer = torch.optim.Adam(self.model.parameters())
 
+        # Loss function
+        self.loss_function = define_loss(self.lossfunc_choice)
 
+        logger.info('Wrapping model with DDP')
         model_ddp = DDP(self.model)
+
         # For GPU
         #model_ddp = DDP(self.model, device_ids=[proc_index])
 
         best_val_loss = float('inf')
-        best_model_wts = None
+        #best_model_wts = None
 
         training_start_time = time.time()
 
@@ -185,66 +194,101 @@ class PINN:
         else:
             alpha = self.final_alpha
 
-        for epoch in range(self.epochs):
-            epoch_start_time = time.time()
+        logger.info('Setting profiler')
+        with torch.profiler.profile(
+                activities=[torch.profiler.ProfilerActivity.CPU],
+                schedule=torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=1),
+                on_trace_ready=torch.profiler.tensorboard_trace_handler('./log'),
+                record_shapes=True
+        ) as profiler: # whole line for profile
 
-            model_ddp.train()  # Set model to training mode
-            running_loss = 0.0
+            for epoch in range(self.epochs):
+                epoch_start_time = time.time()
 
-            if self.dynamic_physics_loss:
-                if self.alpha_epoch_start <= epoch <= self.alpha_epoch_stop:
-                    alpha = alpha + alpha_increments
+                model_ddp.train()  # Set model to training mode
+                running_loss = 0.0
 
-            for inputs, labels in self.train_loader:
-                self.optimizer.zero_grad()  # Clear previous gradients
-                outputs = model_ddp(inputs)  # Forward pass
-                loss = self.loss_function(outputs, labels)  # Calculate loss
+                if self.dynamic_physics_loss:
+                    if self.alpha_epoch_start <= epoch <= self.alpha_epoch_stop:
+                        alpha = alpha + alpha_increments
 
-                physics_loss = physics_loss_fn(outputs, inputs, self.moments)
+                for inputs, labels in self.train_loader:
+                    self.optimizer.zero_grad()  # Clear previous gradients
+                    outputs = model_ddp(inputs)  # Forward pass
+                    loss = self.loss_function(outputs, labels)  # Calculate loss
 
-                # Below I can implement different weights for the monomial physics loss
-                total_loss = (1 - alpha) * loss + alpha * physics_loss.mean()
+                    physics_loss = physics_loss_fn(outputs, inputs, self.moments)
 
-                total_loss.backward()  # Backward pass
-                self.optimizer.step()  # Update labels
+                    # Below I can implement different weights for the monomial physics loss
+                    total_loss = (1 - alpha) * loss + alpha * physics_loss.mean()
 
-                running_loss += loss.item() * inputs.size(0)
+                    total_loss.backward()  # Backward pass
+                    self.optimizer.step()  # Update labels
 
-            avg_training_loss = running_loss / len(self.train_loader.dataset)
-            training_losses.append(avg_training_loss)
+                    running_loss += loss.item() * inputs.size(0)
 
-            # Calculate validation loss after each epoch
-            val_loss = calculate_val_loss(model_ddp, self.val_loader, self.loss_function)
-            validation_losses.append(val_loss)
+                    profiler.step() # Profile
 
-            # If want to free up GPU and calc loss on CPU transfer the operation to the CPU
-            #val_loss = calculate_val_loss(model_ddp.to('cpu'), self.val_loader, self.loss_function)
-            #model_ddp.to(proc_index)
+                avg_training_loss = running_loss / len(self.train_loader.dataset)
+                training_losses.append(avg_training_loss)
 
-            # Save the model if the validation loss improved
-            if val_loss < best_val_loss:
-                best_val_loss = val_loss
-                best_model_wts = model_ddp.state_dict().copy()  # Deep copy the model labels
+                # Calculate validation loss after each epoch
+                val_loss = calculate_val_loss(model_ddp, self.val_loader, self.loss_function)
+                validation_losses.append(val_loss)
 
-            epoch_time = time.time() - epoch_start_time
-            print(
-                f'Epoch {epoch + 1}/{self.epochs} - Loss: {avg_training_loss:.4e}, '
-                f'Validation Loss: {val_loss:.4e}, Time: {epoch_time:.2f}s')
+                # If want to free up GPU and calc loss on CPU transfer the operation to the CPU
+                #val_loss = calculate_val_loss(model_ddp.to('cpu'), self.val_loader, self.loss_function)
+                #model_ddp.to(proc_index)
 
-        self.best_model_wts = best_model_wts
+                # Save the model if the validation loss improved
+                if val_loss < best_val_loss:
+                    best_val_loss = val_loss
+                    best_model_wts = model_ddp.state_dict().copy()  # Deep copy the model labels
+                    #best_model_wts = {k.replace("module.", ""): v for k, v in model_ddp.state_dict().items()}
 
-        # Calculate and print the total training time
-        total_training_time = time.time() - training_start_time
-        print(f'Total training time: {total_training_time:.3f}s')
+                epoch_time = time.time() - epoch_start_time
+                if proc_index == 0:
+                    print(
+                        f'Epoch {epoch + 1}/{self.epochs} - Loss: {avg_training_loss:.4e}, '
+                        f'Validation Loss: {val_loss:.4e}, Time: {epoch_time:.2f}s')
 
-        self.training_loss = training_losses
-        self.val_loss = validation_losses
+            self.best_model_wts = best_model_wts
 
-        # Load best model labels
-        if proc_index == 0 and self.best_model_wts is not None:
-            self.model = model_ddp.load_state_dict(self.best_model_wts)
+            # Calculate and print the total training time
+            total_training_time = time.time() - training_start_time
+            if proc_index == 0:
+                print(f'Total training time: {total_training_time:.3f}s')
 
-        dist.destroy_process_group()
+            self.training_loss = training_losses
+            self.val_loss = validation_losses
+
+            # Save model, training history and attributes
+            if proc_index == 0 and self.best_model_wts is not None:
+                logger.info(f'Process {proc_index} is attempting to save the model.')
+                # Ensure the directory exists
+                os.makedirs(path_to_save, exist_ok=True)
+
+                # Save the PyTorch model's state dict from the `model` attribute
+                model_path = os.path.join(path_to_save, f'{model_type}{model_ID}.pth')
+                consume_pref(self.best_model_wts, prefix="module.")
+
+                # Prepare attributes to save
+                attrs = {
+                    'input_size': self.input_size,
+                    'output_size': self.output_size,
+                    'hidden_layers': self.hidden_layers,
+                    'history': (self.training_loss, self.val_loss)
+                }
+
+                # Save the attributes using pickle
+                attrs_path = os.path.join(path_to_save, f'attrs{model_ID}.pk')
+                with open(attrs_path, 'wb') as f:
+                    pk.dump(attrs, f)
+
+                self.model.load_state_dict(self.best_model_wts)
+                torch.save(self.model.state_dict(), model_path)  # Access the internal `model` attribute
+
+            dist.destroy_process_group()
 
     def predict(self, input):
         self.model.eval()
