4d8481179d69fbb7c16961619f0ef142c8c90c3b
Lucas Starepravo <lucas.gerkenstarepravo@postgrad.macnhester.ac.uk>
2024-11-27 16:21:20 +0000

    adding type hints on class initialisation, saving skip_connections attribute in save_model method in resnet class

--- models/Child_Models.py
+++ models/Child_Models.py
@@ -1,19 +1,11 @@
 import torch.nn as nn
+from torch import Tensor
 from models.NN_Base import BaseModel
 import torch
 
 
-
-def monomial_power_torch(polynomial):
-    monomial_exponent = []
-    for total_polynomial in range(1, polynomial + 1):
-        for i in range(total_polynomial + 1):
-            monomial_exponent.append((total_polynomial - i, i))
-    # Convert list of tuples to a PyTorch tensor
-    return torch.tensor(monomial_exponent, dtype=torch.int)
-
-
 def calc_moments_torch(stand_f, pred_l, n):
+    """ Calculate right-hand side vector of the linear system with the predicted solution vector """
     stand_f = stand_f.view(stand_f.shape[0], n, n)
     pred_l = pred_l.unsqueeze(-1)
     moments = torch.matmul(stand_f, pred_l)
@@ -21,8 +13,19 @@ def calc_moments_torch(stand_f, pred_l, n):
 
 
 class PINN(BaseModel):
-    def __init__(self, alpha, moments_order, hidden_layers, optimizer, loss_function, epochs, batch_size, train_f,
-                 train_l, val_f, val_l):
+    def __init__(self,
+                 alpha: float,
+                 moments_order: int,
+                 hidden_layers: list,
+                 optimizer: str,
+                 loss_function: str,
+                 epochs: int,
+                 batch_size: int,
+                 train_f: Tensor,
+                 train_l: Tensor,
+                 val_f: Tensor,
+                 val_l: Tensor) -> None:
+
         super().__init__(hidden_layers, optimizer, loss_function, epochs, batch_size, train_f, train_l, val_f, val_l)
         self.alpha = alpha
         self.moments_order = int(moments_order)
@@ -30,9 +33,6 @@ class PINN(BaseModel):
     def physics_loss_fn(self, outputs, inputs):
         n = int((self.moments_order ** 2 + 3 * self.moments_order) / 2)
         moments = calc_moments_torch(inputs, outputs, n)
-
-        # target_moments = torch.zeros((outputs.shape[0], n)) #make sure the outputs.shape[0] is capturing the dimension I want
-        # Create the target moments tensor on the same device as the outputs
         target_moments = torch.zeros((outputs.shape[0], n), device=outputs.device)
         target_moments[:, 2] = 1
         target_moments[:, 4] = 1
@@ -48,8 +48,22 @@ class PINN(BaseModel):
 
 
 class ResNet(BaseModel):
-    def __init__(self, skip_connections, hidden_layers, optimizer, loss_function, epochs, batch_size, train_f,
-                 train_l, val_f, val_l):
+    """
+    skip_connections[list]: must be a list of boolean, the first entry of each boolean is the start of the residual
+    block and the second entry is the end of the residual block.
+    """
+    def __init__(self,
+                 skip_connections: list,
+                 hidden_layers: list,
+                 optimizer: str,
+                 loss_function: str,
+                 epochs: int,
+                 batch_size: int,
+                 train_f: Tensor,
+                 train_l: Tensor,
+                 val_f: Tensor,
+                 val_l: Tensor) -> None:
+
         super().__init__(hidden_layers, optimizer, loss_function, epochs, batch_size, train_f, train_l, val_f, val_l)
         self.skip_connections = skip_connections
 
@@ -72,11 +86,11 @@ class ResNet(BaseModel):
         # Use ModuleList to hold all the layers
         return nn.ModuleList(layers)
 
-    def forward_with_ddp(self, model_ddp, inputs): # make sure the predict method of the parent class is using this forward propagation
+    def forward(self, inputs): # make sure the predict method of the parent class is using this forward propagation
         resblock_output = {}
-        for i, layer in enumerate(model_ddp): # self.model was initially self.layers, which is originally the output of the method above
-            res_block = [(index, tup) for index, tup in enumerate(self.scaled_skip_connection) if
-                         i in tup]  # This checks if the current layer is supposed to be a residual block
+        for i, layer in enumerate(self.model): # self.model was initially self.layers, which is originally the output of the method above
+            res_block = [(index, tup) for index, tup in enumerate(self.scaled_skip_connection)
+                         if i in tup]  # This checks if the current layer is supposed to be a residual block
             # For now, this code only works with unique receiving and sending block layers (a layer cannot receive to past outputs, or receive and send the output through the same layer)
             if res_block:
                 if i == res_block[0][1][0]:  # This checks if this is the start or finish of residual block
@@ -88,3 +102,11 @@ class ResNet(BaseModel):
             else:
                 inputs = layer(inputs)
         return inputs
+
+    def save_model(self, path_to_save: str, model_type: str, model_ID: str, **kwargs):
+        """Save the best model weights with additional attributes specific to ResNet."""
+        # Additional attributes specific to this class
+        extra_attrs = {'skip_connections': self.skip_connections}
+        # Call the parent method with additional attributes
+        super().save_model(path_to_save, model_type, model_ID, **extra_attrs)
+
