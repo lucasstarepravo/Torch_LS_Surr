e4e474400a132bd00a69d7295f670617b0a900e3
Lucas Starepravo <lucas.gerkenstarepravo@postgrad.macnhester.ac.uk>
2024-11-27 11:43:55 +0000

    creating sample call of continue_training method

--- models/Resume_Training.py
+++ models/Resume_Training.py
@@ -4,5 +4,9 @@ from models.NN_Base import BaseModel
 if __name__ == '__main__':
     os.environ['MASTER_ADDR'] = 'localhost'
     os.environ['MASTER_PORT'] = '12355'
-    continue_training
-
+    BaseModel.continue_training(nprocs=2,
+                                path_to_save='./data_out',
+                                model_type='pinn',
+                                model_ID='63',
+                                instance_path='./data_out/checkpoint',
+                                epochs=10000)
