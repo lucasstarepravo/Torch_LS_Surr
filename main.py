75d0491d15c8953b0777e5ba4e845a29ec373b36
lucasstarepravo <lucas.gerkenstarepravo@postgrad.manchester.ac.uk>
2024-12-09 16:23:51 +0000

    introducing resume_training variable to fit method, this will be used to move optimiser data to GPU if continue training is True

--- main.py
+++ main.py
@@ -44,7 +44,8 @@ def run_model(path_to_data, layers, model_ID, nprocs, model_type, file_details, 
 
     logger.info('Starting model training')
     mp.spawn(ann.fit,
-             args=(nprocs, path_to_save, model_type, model_ID, train_features, train_labels, val_features, val_labels),
+             args=(nprocs, path_to_save, model_type, model_ID,
+                   train_features, train_labels, val_features, val_labels, False),
              nprocs=nprocs)
 
     evaluate_model(test_features, test_labels, polynomial, model_ID, path_to_save, model_type)
